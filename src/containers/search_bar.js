a909939075e5128c29c465d12f8adfce1eb153fa
loicbrigardis <loic.brigardis@gmail.com>
2016-12-24 01:06:34 +0100

    Use event instead of e

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -19,7 +19,7 @@ class SearchBar extends Component {
   }
 
   onFormSubmit(event) {
-    e.preventDefault();
+    event.preventDefault();
 
     // need to fetch weather data
     this.props.fetchWeather(this.state.term);
